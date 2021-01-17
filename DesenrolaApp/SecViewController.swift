25a4477e82119e4c8c6b1ece55a157ba256fb43d
Luis Pereira <lfbp@academy.cin.ufpe.br>
2021-01-17 12:27:10 -0300

    profile and feedback chalenge

--- DesenrolaApp/SecViewController.swift
+++ DesenrolaApp/SecViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class SecViewController: UIViewController {
 
+    @IBOutlet var firstButton: UIButton!
+    @IBOutlet var secondButton: UIButton!
+    @IBOutlet var thirdButton: UIButton!
     override func viewDidLoad() {
-        print("testing")
         super.viewDidLoad()
+        print("Second View Controller")
+        firstButton.layer.cornerRadius = 0.5 * firstButton.bounds.size.width
+            firstButton.clipsToBounds = true
+        
+        secondButton.layer.cornerRadius = 0.5 * secondButton.bounds.size.width
+            secondButton.clipsToBounds = true
+        
+        thirdButton.layer.cornerRadius = 0.5 * thirdButton.bounds.size.width
+            thirdButton.clipsToBounds = true
         // Do any additional setup after loading the view.
     }
 

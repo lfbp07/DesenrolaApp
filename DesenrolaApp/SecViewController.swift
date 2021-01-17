4f3e0ca02c8de5fdebc2544b60ad9fc7c1fe9bca
Luis Pereira <lfbp@academy.cin.ufpe.br>
2021-01-17 10:04:25 -0300

    scrollview initiated

---
+++ DesenrolaApp/SecViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecViewController.swift
+//  DesenrolaApp
+//
+//  Created by Luis Pereira on 17/01/21.
+//
+
+import UIKit
+
+class SecViewController: UIViewController {
+
+    override func viewDidLoad() {
+        print("testing")
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
